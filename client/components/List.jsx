462c7d858b1f467d75b4b5a0608463f9832926f2
Sam Bowering <samjbowering@gmail.com>
2019-10-28 10:59:36 +1300

    Added basic welcoming divs

---
+++ client/components/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class List extends React.Component {
+
+  render () {
+    return(
+      <div class='list'>
+        
+      </div>
+    )
+  }
+}
+
+export default List
---
+++ client/components/Welcome.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Welcome extends React.Component {
+  render () {
+    return(
+      <>
+        <div class='header'>
+          <button>Sign In</button>
+          <button>Sign Out</button>
+          <button>Register</button>
+        </div>
+        <div class='title-message'>
+          <h1>Not Yet?</h1>
+          <span>Does this sound like you? Jump in and get your 'Not Yet's sorted!</span> 
+        </div>
+      </>
+    )
+  }
+}
+
+export default Welcome
