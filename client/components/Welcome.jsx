300a5fd52555921c0b1d7cf015316c0ce17fd285
Sam Bowering <samjbowering@gmail.com>
2019-10-29 18:36:01 +1300

    Semantic UI React implemented

--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Segment, Button } from 'semantic-ui-react'
 
 import List from './List'
 
@@ -6,15 +7,16 @@ class Welcome extends React.Component {
   render () {
     return(
       <div className='welcome-container'>
-        {/* <div className='header'>
-          <button>Sign In</button>
-          <button>Sign Out</button>
-          <button>Register</button>
-        </div> */}
-        <div className='title-message'>
+        <div className='welcome-header'>
+          <Button>Sign In</Button>
+          <Button>Sign Out</Button>
+          <Button>Register</Button>
+        </div>
+        <div className='welcome-title-message'>
           <h1>Not Yet?</h1>
           <span>Does this sound like you? Jump in and get your 'Not Yet's sorted!</span> 
         </div>
+        <div className='welcome-body-message'></div>
         {/* <List /> */}
       </div>
     )
