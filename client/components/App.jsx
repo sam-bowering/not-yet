64fba945f64b8c953f8bc5c0fd37ce27933f89a2
Sam Bowering <samjbowering@gmail.com>
2019-10-30 11:54:08 +1300

    Added goals and create component routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Welcome from './Welcome'
-import List from './List'
+import Goals from './Goals'
 import Header from './Header'
+import Create from './Create'
 
 const App = () => (
   <Router>
     <Header />
     <Route path='/' exact component={Welcome} />
-    <Route path='/goals' exact component={List} />
+    <Route path='/goals' exact component={Goals} />
+    <Route path='/create' exact component={Create} />
   </Router>
 )
 
