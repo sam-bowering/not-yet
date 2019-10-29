29425051af5ad0d984a1c35b9d67ec8af41d53ae
Sam Bowering <samjbowering@gmail.com>
2019-10-29 19:20:47 +1300

    Save point in styling

--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Segment, Button } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 
 import List from './List'
 
@@ -8,13 +8,15 @@ class Welcome extends React.Component {
     return(
       <div className='welcome-container'>
         <div className='welcome-header'>
-          <Button>Sign In</Button>
+          <Button color='red'>Sign In</Button>
           <Button>Sign Out</Button>
           <Button>Register</Button>
         </div>
-        <div className='welcome-title-message'>
-          <h1>Not Yet?</h1>
-          <span>Does this sound like you? Jump in and get your 'Not Yet's sorted!</span> 
+        <div className='welcome-title'>
+          <div className='welcome-title message'>
+            <h1>Not Yet?</h1>
+            <p>Does this sound like you? Jump in and get your 'Not Yet's sorted!</p>
+          </div>
         </div>
         <div className='welcome-body-message'></div>
         {/* <List /> */}
