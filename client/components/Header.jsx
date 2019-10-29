473bc7499fce63039c2a12900cf752b0c45e13ee
Sam Bowering <samjbowering@gmail.com>
2019-10-30 10:33:09 +1300

    Updated container name appropriately

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,7 +3,7 @@ import { Button } from 'semantic-ui-react'
 
 const Header = () => {
   return(
-    <div className='welcome-header'>
+    <div className='header-container'>
       <Button color='red'>Sign In</Button>
       <Button>Sign Out</Button>
       <Button>Register</Button>
