46f046124fa98482705a009363c68092317d74c2
Sam Bowering <samjbowering@gmail.com>
2019-10-30 09:35:18 +1300

    Added react router dom route setup and link to goal list component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Welcome from './Welcome'
+import List from './List'
 
 const App = () => (
-  <div className='app'>
-    <Welcome />
-  </div>
+  <Router>
+    <Switch>
+      <Route path='/' exact component={Welcome} />
+      <Route path='/goals' exact component={List} />
+    </Switch>
+  </Router>
 )
 
 export default App
