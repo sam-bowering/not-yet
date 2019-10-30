0b24fbc3a3c498e3ba9d61d5b090f5d934646448
Sam Bowering <samjbowering@gmail.com>
2019-10-31 12:38:51 +1300

    Added appropriate functionality to send the goal back up to the client from the database

--- server/routes/list.js
+++ server/routes/list.js
@@ -1,7 +1,9 @@
 const express = require('express')
 
 const router = express()
+
 const db = require('../db/functions/list.db')
+
 router.use(express.json())
 
 router.get('/', (req, res) => {
@@ -15,7 +17,17 @@ router.post('/create', (req, res) => {
   const { name, description, completed, primary, dependants } = req.body
 
   db.addGoal(name, description, completed, primary, dependants)
-    .then(res.sendStatus(200))
+    .then(res.status(200))
+    .then(db.getGoalByName(name)
+      .then(goal => res.json({
+      id: goal.id, 
+      name, 
+      description, 
+      completed: false, 
+      primary: true,
+      dependants
+      }))
+    )
 })
 
 module.exports = router
