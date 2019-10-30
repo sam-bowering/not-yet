e1bda3d2f317bc50b52c3dab01d233300b066250
Sam Bowering <samjbowering@gmail.com>
2019-10-31 09:26:52 +1300

    Added addGoal reducer

--- client/reducers/list.js
+++ client/reducers/list.js
@@ -1,9 +1,19 @@
-import { GET_LIST_SUCCESS } from '../actions/list'
+import { GET_LIST_SUCCESS } from '../actions/getList'
+import { ADD_GOAL_SUCCESS } from '../actions/addGoal'
 
-export default function list (state = [], action) {
+export function list (state = [], action) {
   switch (action.type) {
     case GET_LIST_SUCCESS:
       return action.list
+    default:
+      return state
+  }
+}
+
+export function addGoal (state = {}, action) {
+  switch (action.type) {
+    case ADD_GOAL_SUCCESS:
+      return action.goal
     default:
       return state
   }
