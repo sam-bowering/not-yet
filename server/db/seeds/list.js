b77f3537594ac9b53f3d12ffeafd8244bd6fcc9c
Sam Bowering <samjbowering@gmail.com>
2019-10-31 12:37:58 +1300

    Filled out seeded data in all property fields

--- server/db/seeds/list.js
+++ server/db/seeds/list.js
@@ -2,9 +2,9 @@ exports.seed = function(knex) {
   return knex('list').del()
     .then(function () {
       return knex('list').insert([
-        {id: 1, name: 'Score a goal', description: '', completed: false, primary: false, dependants: 2},
-        {id: 2, name: 'Get the ball', description: '', completed: false, primary: false, dependants: 0},
-        {id: 3, name: 'Kick the ball into the goal', description: '', completed: false, primary: false, dependants: 1}
+        {id: 1, name: 'Score a goal', description: 'Run and score you numpty', completed: false, primary: true, dependants: 2},
+        {id: 2, name: 'Get the ball', description: 'Get the ball you numpty', completed: false, primary: false, dependants: 0},
+        {id: 3, name: 'Kick the ball into the goal', description: 'This is already a score you numpty', completed: false, primary: false, dependants: 1}
       ])
     })
 }
