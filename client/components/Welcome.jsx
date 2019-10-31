bc28909ff28c91bf4ffa2cc10d7c162cc19277fc
Sam Bowering <samjbowering@gmail.com>
2019-10-31 13:49:02 +1300

    Improved welcome page styling

--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Grid } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 import Header from './Header'
@@ -13,16 +13,17 @@ class Welcome extends React.Component {
             <h1>Not Yet?</h1>
             <p>Now's a better time than ever.</p>
           </div>
+          <div className='welcome-buttons'>
+            <Link to='/goals' style={{color: 'white'}}><button type='button'>View</button></Link>
+            <Link to='/create' style={{color: 'white'}}><button type='button'>Create</button></Link>
+          </div>
         </div>
         <div className='welcome-body-message'>
           <Grid columns={3} divided>
             <Grid.Row>
-              <Grid.Column><Link to='/create'>Create</Link></Grid.Column>
-              <Grid.Column>Plan</Grid.Column>
-              <Grid.Column>Achieve</Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column><Link to='/goals'>Jump Back In</Link></Grid.Column>
+              <Grid.Column><Link to='/create'><h1 style={{color: 'rgba(6, 82, 221,1.0)'}}>Create</h1></Link></Grid.Column>
+              <Grid.Column><h1 style={{color: 'rgba(238, 90, 36,1.0)'}}>Plan</h1></Grid.Column>
+              <Grid.Column><h1 style={{color: 'rgba(0, 148, 50,1.0)'}}>Achieve</h1></Grid.Column>
             </Grid.Row>
           </Grid>
         </div>
