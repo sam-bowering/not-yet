d2ca2021ea735abf2340c0aed646750297f656ef
Sam Bowering <samjbowering@gmail.com>
2019-11-02 14:59:03 +1300

    Added more nav links

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -5,7 +5,10 @@ const Header = () => {
   return(
     <div className='header-container'>
       <div className='header-container-text-left'>
-        <span><Link to='/' style={{color: 'white'}}>Home</Link></span>
+        <span className='header-nav-title'><Link to='/' style={{color: 'white'}}>Home</Link></span>
+        <span className='header-nav-divider'>|</span>
+        <span className='header-nav-label'><Link to='/goals' style={{color: 'white'}}>View</Link></span>
+        <span className='header-nav-label'><Link to='/create' style={{color: 'white'}}>Create</Link></span>
       </div>
       <div className='header-container-text-right'>
         <span>Register</span>
