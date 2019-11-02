717546ba655758d5d7a2bf0b4691b58479c10d81
Sam Bowering <samjbowering@gmail.com>
2019-11-02 23:02:20 +1300

    Improved code readability

--- client/components/GoalsList.jsx
+++ client/components/GoalsList.jsx
@@ -15,7 +15,9 @@ const GoalsList = (props) => {
         {props.currentGoals.map(goal =>
         <li>
           <div className='current-goal-item'>
-            <Link to={`/goal/${goal.id}`} onClick={() => handleClick(props, goal.id)}><h1 key={goal.id} className='current-goal-item-title'>{goal.name}</h1></Link>
+            <Link to={`/goal/${goal.id}`} onClick={() => handleClick(props, goal.id)}>
+              <h1 key={goal.id} className='current-goal-item-title'>{goal.name}</h1>
+            </Link>
           </div>
         </li>
         )}
