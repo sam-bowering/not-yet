3413ff67bd6634728e2ae4d4831ea65574c910c4
Sam Bowering <samjbowering@gmail.com>
2019-11-02 22:13:54 +1300

    Added selected goal route

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,6 +5,7 @@ import Welcome from './Welcome'
 import Goals from './Goals'
 import Header from './Header'
 import Create from './Create'
+import SelectedGoal from './SelectedGoal'
 
 const App = () => (
   <Router>
@@ -12,6 +13,7 @@ const App = () => (
     <Route path='/' exact component={Welcome} />
     <Route path='/goals' exact component={Goals} />
     <Route path='/create' exact component={Create} />
+    <Route path='/goal/:selectedGoal' exact component={SelectedGoal}/>
   </Router>
 )
 
