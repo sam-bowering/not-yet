0b2299e00087778075e10020f536c47d04b442ec
Sam Bowering <samjbowering@gmail.com>
2019-11-02 22:21:46 +1300

    Request for specific goal from db added

---
+++ client/api/fetchSelectedGoal.js
@@ -0,0 +1,9 @@
+import makeRequest from './requestor'
+
+export function fetchSelectedGoal (goalName) {
+  return makeRequest(`/list/goal/${goalName}`)
+  .then(res => res.body)
+  .catch(() => { throw new Error('Error returning selected goal') })
+}
+
+export default fetchSelectedGoal
