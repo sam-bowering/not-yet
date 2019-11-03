c2ab1f5d637a028ea2aaa303140d6ca68bc4ae4e
Sam Bowering <samjbowering@gmail.com>
2019-11-04 11:37:29 +1300

    Renamed redux store list state and refactored returned state from delete goal success

--- client/reducers/list.js
+++ client/reducers/list.js
@@ -4,37 +4,37 @@ import { GET_SELECTEDGOAL_SUCCESS } from '../actions/getSelectedGoal'
 import { DELETE_GOAL_SUCCESS } from '../actions/deleteGoal'
 
 const listState = {
-  listItems: []
+  fullList: []
 }
 
-export function listReducer (state = listState, action) {
+export function lists (state = listState, action) {
   switch (action.type) {
     case GET_LIST_SUCCESS:
       const uncompletedList = action.list.filter(listItem => listItem.completed === 0 || listItem.completed === false)
       const completedList = action.list.filter(listItem => listItem.completed === 1 || listItem.completed === true)
       return {
-        listItems: [...action.list],
+        fullList: [...action.list],
         uncompletedList,
         completedList
       }
     case ADD_GOAL_SUCCESS:
       return {
-        listItems: [...state.listItems, action.goal],
+        fullList: [...state.fullList, action.goal],
         uncompletedList: [...state.uncompletedList, action.goal],
         completedList: [...state.completedList]
       }
     case GET_SELECTEDGOAL_SUCCESS:
       return {
-        listItems: [...state.listItems],
+        fullList: [...state.fullList],
         uncompletedList: [...state.uncompletedList],
         selectedGoal: action.selectedGoal,
         completedList: [...state.completedList]
       }
     case DELETE_GOAL_SUCCESS:
       return {
-        listItems: [...state.listItems],
-        uncompletedList: [...state.uncompletedList],
-        completedList: [...state.completedList]
+        fullList: [...action.list],
+        uncompletedList: [...action.list.filter(listItem => listItem.completed === 0 || listItem.completed === false)],
+        completedList: [...action.list.filter(listItem => listItem.completed === 1 || listItem.completed === true)]
       }
     default:
       return state
