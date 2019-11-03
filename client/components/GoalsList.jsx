7ea761d25fdc644b78cc8b70b936feb3014f901d
Sam Bowering <samjbowering@gmail.com>
2019-11-03 16:02:51 +1300

    Removed unnecessary code

--- client/components/GoalsList.jsx
+++ client/components/GoalsList.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-import { getSelectedGoal } from '../actions/getSelectedGoal'
-
 const GoalsList = (props) => {
   return(
     <>
