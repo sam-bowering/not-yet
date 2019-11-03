15a60843ce6d46a77eabd7d291c414091d10cf4a
Sam Bowering <samjbowering@gmail.com>
2019-11-03 15:19:47 +1300

    Created loading indicator

---
+++ client/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Loader } from 'semantic-ui-react'
+
+const Loading = props => {
+  return(
+    <div className='loading-container'>
+      <Loader active size='big' />
+    </div>
+  )
+}
+
+export default Loading
