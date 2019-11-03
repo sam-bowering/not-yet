801ad1572745b3edce6f7ca67926721351864c23
Sam Bowering <samjbowering@gmail.com>
2019-11-04 11:34:34 +1300

    Server now returns the updated list

--- client/api/removeGoal.js
+++ client/api/removeGoal.js
@@ -2,7 +2,7 @@ import makeRequest from './requestor'
 
 export function removeGoal (goalId) {
   return makeRequest('/list/goals', 'delete', { id: goalId })
-    // .then(res => res.body)
+    .then(res => res.body)
     .catch(() => { throw new Error('Error deleting selected goal') })
 }
 
