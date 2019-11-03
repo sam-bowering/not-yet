0ba8773503f492bb65894651c0ab9232deaeff13
Sam Bowering <samjbowering@gmail.com>
2019-11-04 10:47:16 +1300

    Added janky removeGoal api call - needs to be refactored

---
+++ client/api/removeGoal.js
@@ -0,0 +1,9 @@
+import makeRequest from './requestor'
+
+export function removeGoal (goalId) {
+  return makeRequest('/list/goals', 'delete', { id: goalId })
+    // .then(res => res.body)
+    .catch(() => { throw new Error('Error deleting selected goal') })
+}
+
+export default removeGoal
