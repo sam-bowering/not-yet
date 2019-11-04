1f7bb66fe727b6d3a3a203212f5e15c84fe21d86
Sam Bowering <samjbowering@gmail.com>
2019-11-04 20:09:28 +1300

    Added tasks reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 
 import { lists } from './list'
 import { navigation } from './navigation'
+import { tasks } from './tasks'
 
 export default combineReducers({
   lists,
-  navigation
+  navigation,
+  tasks
 })
