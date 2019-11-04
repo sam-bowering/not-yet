9376dd047fe556bc83ae3eac31a3917170328f5e
Sam Bowering <samjbowering@gmail.com>
2019-11-04 20:09:53 +1300

    Added loading to GET TASKS actions

--- client/reducers/navigation.js
+++ client/reducers/navigation.js
@@ -1,14 +1,32 @@
 import { GET_SELECTEDGOAL_PENDING, GET_SELECTEDGOAL_SUCCESS } from '../actions/getSelectedGoal'
+import { GET_TASKS_PENDING, GET_TASKS_SUCCESS } from '../actions/getTasksBySelectedGoal'
 
-export function navigation (state = false, action) {
+const navState = {
+  isLoading: false,
+  tasksLoading: false
+}
+
+export function navigation (state = navState, action) {
   switch (action.type) {
     case GET_SELECTEDGOAL_PENDING:
       return {
-        isLoading: true
+        isLoading: true,
+        tasksLoading: state.tasksLoading
       }
     case GET_SELECTEDGOAL_SUCCESS:
       return {
-        isLoading: false
+        isLoading: false,
+        tasksLoading: state.tasksLoading
+      }
+    case GET_TASKS_PENDING:
+      return {
+        tasksLoading: true,
+        isLoading: state.isLoading
+      }
+    case GET_TASKS_SUCCESS:
+      return {
+        tasksLoading: false,
+        isLoading: state.isLoading
       }
     default:
       return state
