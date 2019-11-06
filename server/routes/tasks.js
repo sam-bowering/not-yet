1da37ba45373898feae2c7e00300d1fb546669f4
Sam Bowering <samjbowering@gmail.com>
2019-11-06 16:17:55 +1300

    Added post route to add task

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -13,4 +13,12 @@ router.get('/goal/:goalId', (req, res) => {
     .then(tasks => res.json(tasks))
 })
 
+router.post('/goal', (req, res) => {
+  const { title, description, goalId } = req.body
+  const completed = 0
+
+  db.addTask(title, description, completed, goalId)
+    .then(task => res.json(task))
+})
+
 module.exports = router
