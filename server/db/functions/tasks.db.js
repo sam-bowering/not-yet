66881d09e2f6147e9ec13d5c67ba937f93ebaa93
Sam Bowering <samjbowering@gmail.com>
2019-11-06 16:17:05 +1300

    Added addTask function to insert to tasks table

--- server/db/functions/tasks.db.js
+++ server/db/functions/tasks.db.js
@@ -5,6 +5,13 @@ function getTasksByGoalId (goalId, db = connection) {
     .where('goal_id', goalId)
 }
 
+function addTask (title, description, completed, goalId, db = connection) {
+  return db('tasks')
+    .where('goal_id', goalId)
+    .insert({ name: title, description, completed, goal_id: goalId })
+}
+
 module.exports = {
-  getTasksByGoalId
+  getTasksByGoalId,
+  addTask
 }
